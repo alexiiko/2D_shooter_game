28e5acccac5c836068a7103554fec1606679f8d0
alexiiko <alexiiko.msl@gmail.com>
2023-06-21 16:38:37 +0200

    added all the needed assets for the game

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 import pygame as pg
 from sys import exit
+import os
+
 
 WIDTH = 650
 HEIGHT = 600
 
-screen = pg.display.set_mode((WIDTH, HEIGHT))
-pg.display.set_caption('Title')
+screen = pg.display.set_mode((0,0), pg.FULLSCREEN)
+pg.display.set_caption('Shooter Game')
 #pg.display.set_icon()
 
 FPS = 60
 
 clock = pg.time.Clock()
 
+player_run = pg.image.load(os.path.join("OneDrive", "Desktop", "shooter_game", "assets", "player", "run.png"))
+
 while True:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             pg.quit()
             exit()
+
+    screen.blit(player_run, (0,0))
 
 
     pg.display.update()
