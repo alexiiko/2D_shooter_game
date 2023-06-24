247506098b57da0981eee15f0b0dd146fcee29c8
alexiiko <alexiiko.msl@gmail.com>
2023-06-24 10:24:41 +0200

    changed player assets from sheets to frames and started implementing animations

--- main.py
+++ main.py
@@ -1,29 +1,54 @@
 import pygame as pg
-from sys import exit
 import os
-
+from sys import exit
 
 WIDTH = 650
 HEIGHT = 600
 
-screen = pg.display.set_mode((0,0), pg.FULLSCREEN)
+screen = pg.display.set_mode((WIDTH, HEIGHT))
 pg.display.set_caption('Shooter Game')
-#pg.display.set_icon()
 
 FPS = 60
 
 clock = pg.time.Clock()
 
-player_run = pg.image.load(os.path.join("OneDrive", "Desktop", "shooter_game", "assets", "player", "run.png"))
+class Player(pg.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+
+        self.run_list = []
+        self.run_list.append(pg.image.load(os.path.join("OneDrive", "Desktop", "shooter_game", "assets", "player", "run_1.png")))
+        self.run_list.append(pg.image.load(os.path.join("OneDrive", "Desktop", "shooter_game", "assets", "player", "run_2.png")))
+        self.run_list.append(pg.image.load(os.path.join("OneDrive", "Desktop", "shooter_game", "assets", "player", "run_3.png")))
+        self.run_list.append(pg.image.load(os.path.join("OneDrive", "Desktop", "shooter_game", "assets", "player", "run_4.png")))
+        self.run_list.append(pg.image.load(os.path.join("OneDrive", "Desktop", "shooter_game", "assets", "player", "run_5.png")))
+
+        self.run_index = 0
+
+        self.idle_list = []
+        self.run_list.append(pg.image.load(os.path.join("OneDrive", "Desktop", "shooter_game", "assets", "idle", "idle_1.png")))
+        self.run_list.append(pg.image.load(os.path.join("OneDrive", "Desktop", "shooter_game", "assets", "idle", "idle_2.png")))
+        self.run_list.append(pg.image.load(os.path.join("OneDrive", "Desktop", "shooter_game", "assets", "idle", "idle_3.png")))
+        self.run_list.append(pg.image.load(os.path.join("OneDrive", "Desktop", "shooter_game", "assets", "idle", "idle_4.png")))
+        self.run_list.append(pg.image.load(os.path.join("OneDrive", "Desktop", "shooter_game", "assets", "idle", "idle_5.png")))
+
+        self.idle_index = 0
+
+        self.image = self.idle_list[self.idle_index]
+
+        self.rect = self.image.get_rect(topleft = (0,0))
+        
+    	
+
+player = pg.sprite.GroupSingle(Player())
 
 while True:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             pg.quit()
             exit()
-
-    screen.blit(player_run, (0,0))
-
+    
+    player.draw(screen)
 
     pg.display.update()
     clock.tick(FPS)
